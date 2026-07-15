430afda71c5aadc7aa3a950505ad12f318b82b4d
woojin984771-star <woojin984771@gmail.com>
2026-07-15 17:00:06 +0900

    chore: remove deprecated eslint config for next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // 빌드 시 에스린트 에러가 있어도 무시하고 배포 진행
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // 빌드 시 타입 에러가 있어도 무시하고 배포 진행
+    // 빌드 시 혹시 모를 타입 에러가 있어도 무시하고 배포 진행
     ignoreBuildErrors: true,
   }
 };
